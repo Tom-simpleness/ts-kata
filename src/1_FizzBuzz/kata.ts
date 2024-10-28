4499f2334d70c6e8b8d44541b76511492eed45ba
ToLan <t.landes84@gmail.com>
2024-10-28 17:17:21 +0100

    kata-fizzbuzz

--- src/1_FizzBuzz/kata.ts
+++ src/1_FizzBuzz/kata.ts
@@ -1,1 +1,12 @@
-export const fizzBuzz = 'something';
+export const fizzBuzz = (count: number): string => {
+  if (count % 15 === 0) {
+    return 'fizzbuzz';
+  }
+  if (count % 3 === 0) {
+    return 'fizz';
+  }
+  if (count % 5 === 0) {
+    return 'buzz';
+  }
+  return count.toString();
+};
--- src/1_FizzBuzz/test.ts
+++ src/1_FizzBuzz/test.ts
@@ -1,7 +1,65 @@
 import { fizzBuzz } from './kata';
 
-describe('Fizz Buzz should', () => {
-  test('do "something"', () => {
-    expect(fizzBuzz).toBe('something');
+describe('calling fizzBuzz()', () => {
+  test('when I fizzbuzz 1 return "1"', () => {
+    //Given
+    const count: number = 1;
+    const expectedResult: string = '1';
+    //When
+    const result: string = fizzBuzz(count);
+    //Then
+    expect(result).toBe(expectedResult);
+  });
+  test('when I fizzbuzz 2 return "2"', () => {
+    //Given
+    const count: number = 2;
+    const expectedResult: string = '2';
+    //When
+    const result: string = fizzBuzz(count);
+    //Then
+    expect(result).toBe(expectedResult);
+  });
+  test('when I fizzbuzz 4 return "4"', () => {
+    //Given
+    const count: number = 4;
+    const expectedResult: string = '4';
+    //When
+    const result: string = fizzBuzz(count);
+    //Then
+    expect(result).toBe(expectedResult);
+  });
+  test('when I fizzbuzz 3 return "fizz"', () => {
+    //Given
+    const count: number = 3;
+    const expectedResult: string = 'fizz';
+    //When
+    const result: string = fizzBuzz(count);
+    //Then
+    expect(result).toBe(expectedResult);
+  });
+  test('when I fizzbuzz 5 return "buzz"', () => {
+    //Given
+    const count: number = 5;
+    const expectedResult: string = 'buzz';
+    //When
+    const result: string = fizzBuzz(count);
+    //Then
+    expect(result).toBe(expectedResult);
+  });
+});
+
+describe('calling fizzBuzz() one block', () => {
+  it.each([
+    [1, '1'],
+    [2, '2'],
+    [3, 'fizz'],
+    [4, '4'],
+    [5, 'buzz'],
+    [15, 'fizzbuzz'],
+  ])('when I fizzbuzz %i return "%s"', (input, expected) => {
+    //When
+    const result: string = fizzBuzz(input);
+    //Then
+    expect(result).toBe(expected);
   });
 });
