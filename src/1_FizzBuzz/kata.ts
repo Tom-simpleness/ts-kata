e05af0c3184c8073a1be85a9cf534bb62c8991ec
ToLan <t.landes84@gmail.com>
2024-10-28 17:19:51 +0100

    fizzbuzz refacto

--- src/1_FizzBuzz/kata.ts
+++ src/1_FizzBuzz/kata.ts
@@ -10,3 +10,14 @@ export const fizzBuzz = (count: number): string => {
   }
   return count.toString();
 };
+
+export const fizzBuzzRefacto = (count: number): string => {
+  const rules = [
+    { divisor: 15, result: 'fizzbuzz' },
+    { divisor: 3, result: 'fizz' },
+    { divisor: 5, result: 'buzz' },
+  ];
+
+  const match = rules.find((rule) => count % rule.divisor === 0);
+  return match?.result ?? count.toString();
+};
